9ffbc42d8b87644fd6828e718945ea82c567c28d
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-02-16 17:36:12 +0100

    Added footnote command
    Signed-off-by: Davide Valsecchi <valsecchi.davide94@gmail.com>

---
+++ Blocks/NoteBlocks.py
@@ -0,0 +1,34 @@
+import logging
+from . import CommandParser
+from .Block import *
+
+class FootnoteBlock(Block):
+	'''This class handles \footnote command'''
+
+	@staticmethod
+	def parse(parser, tex, parent_block, options):
+		#we get the content and the left_tex
+		params, left_tex = CommandParser.parse_options(tex,
+			[('content','{','}')])
+		content = params['content']
+		#we first create the Block
+		block = FootnoteBlock(content, parent_block)
+		logging.debug('FootnoteBlock.parse @')
+		#now we parse the content 
+		poptions = {'parse_sections':False,
+                   'parse_envs':True,
+                   'parse_math':True,
+                   'parse_commands':True }
+		children_blocks = parser.parser_cycle(content, block, poptions)
+		#now we can add the children nodes
+		block.add_children_blocks(children_blocks)
+		return (block, left_tex)
+
+
+	def __init__(self, tex, parent_block):
+		super().__init__('footnote', tex, parent_block)
+
+
+parser_hooks = {
+	'footnote' : FootnoteBlock.parse,
+}
