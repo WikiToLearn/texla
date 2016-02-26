c37293dfece03aa89ea478e41ed195fad3cb972b
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-02-26 15:27:52 +0100

    Handled block for dots

---
+++ Blocks/DotBlock.py
@@ -0,0 +1,25 @@
+import logging
+from . import CommandParser
+from .Block import *
+
+class DotsBlock(Block):
+
+    @staticmethod
+    def parse(parser, tex, parent_block, params):
+        dot_type = params['cmd']
+        block = DotsBlock(dot_type, parent_block)
+        return (block, tex)
+
+    def __init__(self, dot_type, parent_block):
+        super().__init__('dots', '', parent_block)
+        self.dot_type = dot_type
+        self.attributes['dot_type'] = dot_type
+
+    def __str__(self):
+        return '<Block:{}, ID:{}, dot_type:{}>'.format(
+                self.block_name, self.id, self.dot_type)
+
+parser_hooks = {
+    'dots' : DotsBlock.parse,
+    'ldots' : DotsBlock.parse
+}
