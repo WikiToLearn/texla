4457229164e6fa8495b7b3e1d92d93cde0d97049
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-03-01 15:38:25 +0100

    Added centering support

--- Blocks/AlignmentBlocks.py
+++ Blocks/AlignmentBlocks.py
@@ -3,32 +3,39 @@ from .Utilities import *
 from .Block import Block
 
 class AlignmentBlock(Block):
-	'''This class handles the flushright, flushleft
-	and center environments'''
+    '''This class handles the flushright, flushleft
+    and center environments'''
 
-	@staticmethod
-	def parse(parser, tex, parent_block, params):
-		#we first create the Block
-		block = AlignmentBlock(params['env'], tex, parent_block)
-		logging.debug('AlignmentBlock.parse @ type: %s', params['env'])
-		#now we parse the content
-		children_blocks = parser.parse_instructions(tex, block, {})
-		#now we can add the children nodes
-		block.add_children_blocks(children_blocks)
-		return block
+    @staticmethod
+    def parse_env(parser, tex, parent_block, params):
+        #we first create the Block
+        block = AlignmentBlock(params['env'], tex, parent_block)
+        logging.debug('AlignmentBlock.parse_env @ type: %s', params['env'])
+        #now we parse the content
+        children_blocks = parser.parse_instructions(tex, block, {})
+        #now we can add the children nodes
+        block.add_children_blocks(children_blocks)
+        return block
+
+    @staticmethod
+    def parse_command(parser, tex, parent_block, params):
+        block = AlignmentBlock(params['cmd'], '' , parent_block)
+        logging.debug('AlignmentBlock.parse_cmd @ type: %s', params['cmd'])
+        return (block,tex)
 
 
-	def __init__(self, align_type, tex, parent_block):
-		super().__init__(align_type, tex, parent_block)
-		self.attributes['align_type'] = align_type
+    def __init__(self, align_type, tex, parent_block):
+        super().__init__(align_type, tex, parent_block)
+        self.attributes['align_type'] = align_type
 
-	def __str__(self):
-		return '<Block:{}, ID:{}, alignment:{}>'.format(
-			self.block_name, self.id, self.attributes['align_type'])
+    def __str__(self):
+        return '<Block:{}, ID:{}, alignment:{}>'.format(
+            self.block_name, self.id, self.attributes['align_type'])
 
 
 parser_hooks = {
-	'flushleft' : AlignmentBlock.parse,
-	'flushright' : AlignmentBlock.parse,
-	'center' : AlignmentBlock.parse
+    'flushleft' : AlignmentBlock.parse_env,
+    'flushright' : AlignmentBlock.parse_env,
+    'center' : AlignmentBlock.parse_env,
+    'centering' : AlignmentBlock.parse_command
 }
