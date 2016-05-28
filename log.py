d406e5418436a63d285576eaec8cbae0cd1833a8
Riccardo Iaconelli <riccardo@kde.org>
2016-05-28 12:47:00 +0200

    set info as default debug level
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- log.py
+++ log.py
@@ -1,10 +1,15 @@
 import logging
 import sys
-loglevel = sys.argv[1].split('=')[1]
-if len(sys.argv)>2:
-	output =  sys.argv[2].split('=')[1] in ('True','true','Y','y','yes','Yes')
+
+if len(sys.argv) > 1:
+    loglevel = sys.argv[1].split('=')[1]
 else:
-	output = False
+    loglevel = "info"
+
+if len(sys.argv)>2:
+    output =  sys.argv[2].split('=')[1] in ('True','true','Y','y','yes','Yes')
+else:
+    output = False
 
 numeric_level = getattr(logging, loglevel.upper(), None)
 if not isinstance(numeric_level, int):
