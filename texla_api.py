4d2a20cdca88507b3e2860825e22cc43ba790135
Alessandro Tundo <alessandrotundo94@gmail.com>
2016-06-04 15:10:47 +0200

    Fix log issue, host on localhost, add / route
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- texla_api.py
+++ texla_api.py
@@ -1,17 +1,20 @@
+import log
 from flask import Flask, request, jsonify
 import json
 import os, random, string
 from texla.Parser import Parser
 from texla.Renderers.MediaWikiRenderer import MediaWikiRenderer
 
-
 app = Flask('texla_api')
 app.secret_key = 'abcdef'
 
+@app.route('/')
+def welcome():
+    return "Welcome to TeXLa!"
+
 @app.route('/texla')
 def helloworld():
-    return "hello world!"
-
+    return "Start to import :)"
 
 if __name__ == '__main__':
-        app.run(host="0.0.0.0", port=5000, debug=True)
+        app.run(host="127.0.0.1", port=5000, debug=True)
