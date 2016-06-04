7dcaa9bc9d7947e155ab5eb1e1d93acc8eb38394
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-06-04 17:27:26 +0200

    Import of Block fully generic
    The dynamic import of block failed if the texla lib was imported inside
    another lib. Now the import is fully generic.

--- texla/Parser/Blocks/__init__.py
+++ texla/Parser/Blocks/__init__.py
@@ -11,12 +11,13 @@ parser_hooks={}
 not_import = ('__init__.py', 'utility.py', 'DocumentBlock.py',
     'CommandParser.py', 'MacroParser.py')
 
-for module in os.listdir('texla/Parser/Blocks'):
+#getting path of the modules
+for module in os.listdir(os.path.dirname(__file__)):
     #utility module is not imported under Blocks
     if module in not_import:
         continue
     if module.endswith('.py'):
-        m = importlib.import_module('texla.Parser.Blocks.'+ module[:-3])
+        m = importlib.import_module(__name__+'.'+ module[:-3])
         logging.debug('BLOCKS_MODULE @ imported %s', m.__name__)
         if hasattr(m,"parser_hooks"):
             for key,value  in m.parser_hooks.items():
