22b6d482f78c88298555cd7113422b5d414fd436
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-11-03 14:16:57 +0100

    Fixed space_check plugin with a regex.
    Now also multiple spaces after \n are removed.

--- texla/Renderers/plugins/spaces_check.py
+++ texla/Renderers/plugins/spaces_check.py
@@ -1,11 +1,12 @@
 import logging
+import re
 
 
 def check_space(block, text):
     logging.debug("Plugin spaces_check @  checking output of block {}".
                   format(block.id))
-    text = text.replace("\n ", "\n")
-    return text
+    r = re.compile(r'\n[ ]*')
+    return r.sub("\n", text)
 
 
 plugin_render_hooks = {
